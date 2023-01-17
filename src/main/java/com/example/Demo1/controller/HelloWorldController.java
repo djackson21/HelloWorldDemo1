442d96650ab99c1f5f3cf28a13a45bf11e40edc3
Derron Jackson <Derrnjackson01gmail.com>
2023-01-17 10:16:14 -0600

    Test

--- src/main/java/com/example/Demo1/controller/HelloWorldController.java
+++ src/main/java/com/example/Demo1/controller/HelloWorldController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
 
-    @GetMapping("/hello")
     public String HelloWorld() {
         return "Hello World";
     }
